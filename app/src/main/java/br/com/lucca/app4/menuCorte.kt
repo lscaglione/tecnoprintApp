6e32f4e01dc9f6835e42eeec91ae0bcae30a5afb
lucca <luccascaglione1@gmail.com>
2023-11-23 20:32:00 -0300

    Inserido duas activitys menuCorte e inserirCorte, layout pronto das duas

---
+++ app/src/main/java/br/com/lucca/app4/menuCorte.kt
@@ -0,0 +1,11 @@
+package br.com.lucca.app4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menuCorte : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_corte)
+    }
+}
