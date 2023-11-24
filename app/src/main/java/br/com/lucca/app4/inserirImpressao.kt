5752ae8df5e7afc25428c847731d61c908481754
lucca <luccascaglione1@gmail.com>
2023-11-24 07:20:57 -0300

    Mudança no layout principal, e mudança de nomes da activity main para activity_menu_impressao.xml

--- app/src/main/java/br/com/lucca/app4/inserirImpressao.kt
+++ app/src/main/java/br/com/lucca/app4/inserirImpressao.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.android.material.textfield.TextInputEditText
 
 class inserirImpressao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +16,7 @@ class inserirImpressao : AppCompatActivity() {
 
 
         botaoTelaInicial.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, menuImpressao::class.java)
             startActivity(intent)
 
         }
--- app/src/main/java/br/com/lucca/app4/menuImpressao.kt
+++ app/src/main/java/br/com/lucca/app4/menuImpressao.kt
@@ -4,12 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class menuImpressao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu_impressao)
 
         val botao1: Button = findViewById(R.id.botao1)
         botao1.setOnClickListener {
