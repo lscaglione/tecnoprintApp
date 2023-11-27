cf13d3f4983be0404cff07e5bb45855cb273f69f
lucca <luccascaglione1@gmail.com>
2023-11-26 23:12:54 -0300

    Mudança de layout, implementação de opcões de máquinas para o usuário

--- app/src/main/java/br/com/lucca/app4/inserirImpressao.kt
+++ app/src/main/java/br/com/lucca/app4/inserirImpressao.kt
@@ -10,51 +10,71 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 
 
 class inserirImpressao : AppCompatActivity() {
 
+    lateinit var editTextFuncionario: EditText
+    lateinit var editTextProduto: EditText
+    lateinit var botaoConfirma: Button
+    lateinit var selecionarMaquina: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inserir_impressao)
 
+
+        editTextFuncionario = findViewById(R.id.selecionarFuncionario)
+        editTextProduto = findViewById(R.id.selecionarProduto)
+        botaoConfirma = findViewById(R.id.botaoConfirmar)
+        selecionarMaquina = findViewById(R.id.selecionarMaquina)
+
+        botao()
+        botaoConfirma()
+        listaDeMaquinasLista()
+
+
+    }
+
+
+    private fun botao() {
         val botaoTelaInicial: Button = findViewById(R.id.botaoTelaInicial)
         botaoTelaInicial.setOnClickListener {
             intent = Intent(this, menuImpressao::class.java)
             startActivity(intent)
         }
+    }
 
-        val editTextFuncionario: EditText = findViewById(R.id.selecionarFuncionario)
-        val editTextProduto: EditText = findViewById(R.id.selecionarProduto)
-        val editTextMaquina: EditText = findViewById(R.id.selecionarMaquina)
-        val botaoConfirma: Button = findViewById(R.id.botaoConfirmar)
-
+    private fun botaoConfirma() {
+        //Usando o getSharedPreferences para modificar e guardar os dados digitados
         val preferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE)
         val editor = preferences.edit()
 
-
-
-
+        //Aplicando a lógica pra quando o usuário apertar em confirmar para obter os dados
         botaoConfirma.setOnClickListener {
             val funcionarioDigitado = editTextFuncionario.text.toString()
             editor.putString("funcionarioNovo", funcionarioDigitado)
             editor.apply()
 
-
             val produtoDigitado = editTextProduto.text.toString()
             editor.putString("produtoNovo", produtoDigitado)
             editor.apply()
 
-            val maquinaDigitado = editTextMaquina.text.toString()
-            editor.putString("maquinaNovo", maquinaDigitado)
+            val maquinaEscolhida = selecionarMaquina.selectedItem.toString()
+            editor.putString("maquinaEscolhida", maquinaEscolhida)
             editor.apply()
-
-            val intent = Intent(this, menuImpressao::class.java)
-            startActivity(intent)
+//
         }
     }
 
+    private fun listaDeMaquinasLista() {
+        val listaMaquinas = arrayOf("Selecione a maquina", "1", "2", "3", "4", "5", "6")
+        val adaptar = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, listaMaquinas)
+        selecionarMaquina.setAdapter(adaptar)
+    }
 
 }
+
 
--- app/src/main/java/br/com/lucca/app4/menuImpressao.kt
+++ app/src/main/java/br/com/lucca/app4/menuImpressao.kt
@@ -4,44 +4,44 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class menuImpressao : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_impressao)
 
-
         botoes()
         pegaProduto()
         pegaFuncionario()
+        pegaMaquina()
 
     }
 
+    //Funções para exibir o valor digitado no menu inserirImpressao
     private fun pegaFuncionario() {
         val funcionario1: TextView = findViewById(R.id.funcionario1)
         val preferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE)
         val funcionarioSalvo = preferences.getString("funcionarioNovo", "")
         funcionario1.text = funcionarioSalvo
-
     }
 
     private fun pegaProduto() {
         val produto1: TextView = findViewById(R.id.produto1)
         val preferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE)
         val produtoSalvo = preferences.getString("produtoNovo", "")
-
         produto1.text = produtoSalvo
-
     }
 
-//    private fun pegaMaquina(){
-//        val maquina1: TextView = findViewById(R.id.)
-//
-//    }
+    private fun pegaMaquina(){
+        val maquina1: TextView = findViewById(R.id.maquina1)
+        val preferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE)
+        val maquinaSalvo = preferences.getString("maquinaEscolhida", "")
+        maquina1.text = maquinaSalvo
+    }
 
     private fun botoes() {
         val botao1: Button = findViewById(R.id.botaoInserirImpressao)
@@ -62,6 +62,4 @@ class menuImpressao : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-
 }
