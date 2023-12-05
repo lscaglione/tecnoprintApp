ccbebdbfa58e82b53903165403d9a8752a5db63f
lucca <luccascaglione1@gmail.com>
2023-12-05 08:26:46 -0300

    Início de funcionalidades do menuCorte e inserirCorte

--- app/src/main/java/br/com/lucca/app4/inserirCorte.kt
+++ app/src/main/java/br/com/lucca/app4/inserirCorte.kt
@@ -1,13 +1,73 @@
 package br.com.lucca.app4
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 
 class inserirCorte : AppCompatActivity() {
+
+    lateinit var selecionarMaquinaCorte: Spinner
+    lateinit var selecionarProdutoCorte1: EditText
+    lateinit var selecionarProdutoCorte2: EditText
+    lateinit var selecionarProdutoCorte3: EditText
+    lateinit var selecionarProdutoCorte4: EditText
+    lateinit var selecionarProdutoCorte5: EditText
+    lateinit var selecionarProdutoCorte6: EditText
+    lateinit var selecionarProdutoCorte7: EditText
+    lateinit var selecionarProdutoCorte8: EditText
+    lateinit var botaoConfirma: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inserir_corte)
+
+
+        selecionarMaquinaCorte = findViewById(R.id.spinnerSelecionarMaquinaCorte)
+        selecionarProdutoCorte1 = findViewById(R.id.inserirProdutoCorte1)
+        selecionarProdutoCorte2 = findViewById(R.id.inserirProdutoCorte2)
+        selecionarProdutoCorte3 = findViewById(R.id.inserirProdutoCorte3)
+        selecionarProdutoCorte4 = findViewById(R.id.inserirProdutoCorte4)
+        selecionarProdutoCorte5 = findViewById(R.id.inserirProdutoCorte5)
+        selecionarProdutoCorte6 = findViewById(R.id.inserirProdutoCorte6)
+        selecionarProdutoCorte7 = findViewById(R.id.inserirProdutoCorte7)
+        selecionarProdutoCorte8 = findViewById(R.id.inserirProdutoCorte8)
+        botaoConfirma = findViewById(R.id.botaoConfirmarCorte1)
+
+        botaoConfirma()
+        listaDeMaquinasCorte()
+
+    }
+
+    private fun botaoConfirma(){
+        val preferences = getSharedPreferences("SharedPreferencesCortes", Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+
+        botaoConfirma.setOnClickListener {
+            val corteSelecionado = selecionarProdutoCorte1.text.toString()
+            editor.putString("produtoCorte1", corteSelecionado)
+            editor.apply()
+            Toast.makeText(this, "Dados atualizados", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+    private fun listaDeMaquinasCorte (){
+        val listaMaquinasCorte = arrayOf(
+            "Selecione a maquina",
+            "Maquina 1",
+            "Maquina 2"
+        )
+
+        val adaptar = ArrayAdapter(this, R.layout.spinner_style, listaMaquinasCorte)
+        adaptar.setDropDownViewResource(R.layout.dropdown_spinner)
+        selecionarMaquinaCorte.adapter = adaptar
+
     }
 }
--- app/src/main/java/br/com/lucca/app4/menuCorte.kt
+++ app/src/main/java/br/com/lucca/app4/menuCorte.kt
@@ -1,14 +1,29 @@
 package br.com.lucca.app4
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class menuCorte : AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        // Atualiza os dados toda vez que a Activity é retomada.
+        atualizarDados()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_corte)
+
+        botaoMenuImpressao()
+        atualizarDados()
+    }
+
+    fun botaoMenuImpressao() {
 
         val button: Button = findViewById(R.id.botaTelaInicialCorteParaMenu)
 
@@ -17,5 +32,16 @@ class menuCorte : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+
+    }
+
+    fun atualizarDados() {
+        val preferences = getSharedPreferences("SharedPreferencesCortes", Context.MODE_PRIVATE)
+
+        val produtoCorteSelecionado1 = preferences.getString("produtoCorte1", "")
+        findViewById<TextView?>(R.id.produtoCorte1).text = produtoCorteSelecionado1
+
+
     }
 }
